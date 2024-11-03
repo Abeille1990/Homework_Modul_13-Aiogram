7495d1449daa82cc5b309e9178d42e9e2d98a7b3
Abeille1990 <tatiana.tyukova@gmail.com>
2024-11-03 16:18:05 +0300

    Add files via upload

--- module_13_1.py
+++ module_13_1.py
@@ -1,34 +1,18 @@
 import asyncio
 
 
-async def start_strongman1(name, power):
+async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for i in range(1, 6):
+        await asyncio.sleep(1 / power)
         print(f'Силач {name} поднял {i} шар.')
-        await asyncio.sleep(5/power)
-    print(f'Силач {name} закончил соревнования.')
-
-
-async def start_strongman2(name, power):
-    print(f'Силач {name} начал соревнования.')
-    for i in range(1, 6):
-        print(f'Силач {name} поднял {i} шар.')
-        await asyncio.sleep(5/power)
-    print(f'Силач {name} закончил соревнования.')
-
-
-async def start_strongman3(name, power):
-    print(f'Силач {name} начал соревнования.')
-    for i in range(1, 6):
-        print(f'Силач {name} поднял {i} шар.')
-        await asyncio.sleep(5/power)
     print(f'Силач {name} закончил соревнования.')
 
 
 async def start_tournament():
-    task1 = asyncio.create_task(start_strongman1("Pasha", 3))
-    task2 = asyncio.create_task(start_strongman2("Denis", 4))
-    task3 = asyncio.create_task(start_strongman3('Apollon', 5))
+    task1 = asyncio.create_task(start_strongman("Pasha", 3))
+    task2 = asyncio.create_task(start_strongman("Denis", 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
     await task1
     await task2
     await task3
